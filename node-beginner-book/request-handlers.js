edb6a478e3ef9d0b3db7fc8b936a13cce1d1dc17
libertinistic <libertinistic@gmail.com>
2016-07-17 20:12:31 -0700

    midway into coding POST capabilities

--- node-beginner-book/request-handlers.js
+++ node-beginner-book/request-handlers.js
@@ -1,20 +1,33 @@
-const exec = require('child_process').exec;
+var exec = require('child_process').exec;
 
-function start() {
+function start(response) {
   console.log('Request handler "start" was called.');
-  var content = 'empty';
+
+  var body = '<html>' +
+    '<head>' +
+    '<meta http-equiv="Content-Type" content="text/html; ' +
+    'charset=UTF-8" />' +
+    '</head>' +
+    '<body>' +
+    '<form action="/upload" method="post">' +
+    '<textarea name="text" rows="20" cols="60"></textarea>' +
+    '<input type="submit" value="Submit text" />' +
+    '</form>' +
+    '</body>' +
+    '</html>';
 
   exec('ls -lah', function (error, stdout, stderr) {
-    content = stdout;
-    // console.log(content);
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(body);
+    response.end();
   });
-
-  return content;
 }
 
-function upload() {
+function upload(response) {
   console.log('Request handler "upload" was called.');
-  return 'Hello Upload';
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.write('Hello Upload');
+  response.end();
 }
 
 exports.start = start;
--- node-beginner-book/server.js
+++ node-beginner-book/server.js
@@ -4,17 +4,13 @@ const url = require('url');
 function start(route, handle) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
-    console.log('Request Receivef For: ' + pathname);
+    console.log('Request for ' + pathname + ' received.');
 
-    var content = route(handle, pathname);
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write(content);
-    response.end();
+    route(handle, pathname, response);
   }
 
   http.createServer(onRequest).listen(8888);
-
-  console.log('Server Initiated.');
+  console.log('Server has started.');
 }
 
 exports.start = start;

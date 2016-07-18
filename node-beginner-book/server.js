da31d7602d4d9715f5b718e98bac48ef90184f95
libertinistic <libertinistic@gmail.com>
2016-07-17 17:54:58 -0700

    this one contains some questions about how the code was originally written in the book

--- node-beginner-book/server.js
+++ node-beginner-book/server.js
@@ -1,12 +1,16 @@
 const http = require('http');
 const url = require('url');
+const router = require('./router'); //Provided code doesn't work without this?
 
 function start() {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
     console.log('Request Receivef For: ' + pathname);
+
+    router.route(pathname); //This needed router. to run.
+
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('Hello World');
+    // response.write('<p>Hello World</p>');
     response.end();
   }
 
